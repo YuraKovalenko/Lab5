21a502e4ee48d65699b131aa9be7eaa6613125e5
YuraKovalenko <57789851+YuraKovalenko@users.noreply.github.com>
2020-04-17 15:31:06 +0300

    Add files via upload

---
+++ OOP.Lab5.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+void Solve();
+
+class Point//базовий клас
+{
+protected://модифікатор доступу
+	float x, y;
+public:
+	Point(float ValueX)
+	{
+		x = ValueX;
+		y = x;
+	}
+	float Length()
+	{
+		return sqrt(pow(x, 2) + pow(y, 2));
+	}
+};
+
+class SecondPoint :public Point//похідний клас
+{
+private :
+	char s;
+public:
+	SecondPoint(char valueS) : Point(valueS)//похідний конструктор створений від базового
+	{
+		s = valueS;
+	}
+	/*void Set(char ValueS)
+	{
+		s = ValueS;
+	}
+	int Get()
+	{
+		return s;
+	}
+	*/
+	void foo()
+	{
+		char arr[6]{'q','w','e','r','t','y',};
+		char s1;
+		for (int i = 0; i < 6; i++)
+		{
+			cout << arr[i] << setw(3);
+		}
+		cout << "\n";
+		cout << "Enter s1 for replace: ";
+		cin >> s1;
+		for (int i = 0; i < 6; i++)
+		{
+			if (arr[i] == s)
+			{
+				arr[i] = s1;
+			}
+		}
+		for (int i = 0; i < 6; i++)
+		{
+			cout << arr[i] << setw(3);
+		}
+		cout << "\n";
+	}
+};
+
+int main()
+{
+	Solve();
+}
+
+void Solve()//функція вирішення
+{
+	cout << "Kovalenko Yura IS-92\n";
+	cout << "My string is:" << "\n";
+	cout << "q w e r t y\n";
+	char symbol;
+	cout << "Enter symbol : ";
+	cin >> symbol;
+	if (symbol == 'q' || symbol == 'w' || symbol == 'e' || symbol == 'r' || symbol == 't' || symbol == 'y')
+	{
+		SecondPoint a(symbol);
+		a.foo();
+		Point b(2);
+		cout << "Length is : " << b.Length() << "\n";
+	}
+	else
+	{
+		cout << "ERROR!!!";
+	}
+	cout << "\n";
+	system("pause");
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.Csharp
+{
+    class Program
+    {
+        class Point
+        {
+            protected double x, y;
+            public Point(double ValueX,double ValueY)
+            {
+                x = ValueX;
+                y = ValueY;
+            }
+
+            public double Length()
+            {
+                return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            }
+        }
+
+        class SecondPoint : Point
+        {
+            private char S;
+            public SecondPoint(double x, double y, char s) : base(x,y)
+            {
+                this.S = s;
+            }
+            public void Foo()
+            {
+                char[] Arr = { 'q', 'w', 'e', 'r', 't', 'y', };
+                for (int i = 0; i < 6; i++) 
+                {
+                    Console.WriteLine(" " + Arr[i]);
+                }
+                //Console.WriteLine("\n");
+                Console.WriteLine("Enter s1 for replace: ");
+                char s1 = Convert.ToChar(Console.ReadLine());
+                for (int i = 0; i < 6; i++)
+                {
+                    if(Arr[i]==S)
+                    {
+                        Arr[i] = s1;
+                    }
+                }
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.WriteLine(" " + Arr[i]);
+                }
+            }
+        }
+
+        static void Solve()
+        {
+            Console.WriteLine("My string is:");
+            Console.WriteLine("q w e r t y");
+            Console.WriteLine("Enter symbol for replace:");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            if (symbol == 'q' || symbol == 'w' || symbol == 'e' || symbol == 'r' || symbol == 't' || symbol == 'y')
+            {
+                SecondPoint a = new SecondPoint(3, 4, symbol);
+                a.Foo();
+                Console.WriteLine("Enter X:");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Y:");
+                double y = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Length is: " + a.Length());
+            }
+            else
+            {
+                Console.WriteLine("ERROR!!!\n");
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Solve();
+        }
+    }
+}
