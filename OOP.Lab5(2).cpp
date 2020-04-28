68fd9902b6a2c77f8ab2324e0d34e02df426def0
YuraKovalenko <57789851+YuraKovalenko@users.noreply.github.com>
2020-04-29 01:19:30 +0300

    Add files via upload

---
+++ OOP.Lab5(2).cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void Solve();
+
+class Figures//похідний клас
+{
+protected:
+	double a, b, r, pi = 3.14;
+public :
+	Figures(double x)
+	{
+		r = x;
+	}
+	Figures(double x, double y)
+	{
+		a = x;
+		b = y;
+	}
+	virtual void Perimeter()
+	{
+		double p = 2 * (a + b);
+		cout << "Perimetr is:" << p;
+	}
+	virtual void Area()
+	{
+		double s = a * b;
+		cout << "Area is:" << s;
+	}
+};
+
+class Rectangle : Figures//похідний клас
+{
+private:
+	double x, y;
+public:
+	Rectangle(double x, double y) : Figures(x, y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+	double Length()
+	{
+		return sqrt(pow(x, 2) + pow(y, 2));
+	}
+	virtual void Perimeter() 
+	{
+		double a = sqrt(pow(x, 2) + pow(y, 2));
+		double b = sqrt(pow(x, 2) + pow(y, 2));
+		double p = 2 * (a + b);
+		cout << "Perimetr is:" << p;
+	}
+	virtual void Area()
+	{
+		double a = sqrt(pow(x, 2) + pow(y, 2));
+		double b = sqrt(pow(x, 2) + pow(y, 2));
+		double s = a * b;
+		cout << "Area is:" << s;
+	}
+};
+
+class Circle : Figures//похідний клас
+{
+protected:
+	double x;
+public:
+	Circle(double r) : Figures(x)
+	{
+		this->x = r;
+	}
+	virtual void Perimeter()
+	{
+		double p = 2 * pi *x;
+		cout << "Perimetr is:" << p;
+	}
+	virtual void Area()
+	{
+		double s = pi * x*x;
+		cout << "Area is:" << s;
+	}
+};
+
+int main()
+{
+	Solve();
+}
+
+void Solve()
+{
+	cout << "Kovalenko Yura IS-92\n";
+	Rectangle a(3, 4);
+	cout << "For Rectangle\n";
+	a.Area();
+	cout << "\n";
+	a.Perimeter();
+	cout << "\n";
+	Circle b(5);
+	cout << "For Circle\n";
+	b.Area();
+	cout << "\n";
+	b.Perimeter();
+	cout << "\n";
+	system("pause");
+}
+
---
+++ Program2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Lab5_2_Csharp
+{
+    class Program
+    {
+        class Figures//базовий клас
+        {
+            protected double a, b, r, pi = 3.14;
+            public Figures(double x)
+            {
+                r = x;
+            }
+            public Figures(double x, double y)
+            {
+                a = x;
+                b = y;
+            }
+            //віртуальні функції
+            public virtual void Perimeter()
+            {
+                double p = 2 * (a + b);
+                Console.WriteLine("Perimetr is:" + p);
+            }
+            public virtual void Area()
+            {
+                double s = a * b;
+                Console.WriteLine("Area is:" + s);
+            }
+        }
+
+        class Rectangle : Figures//похідний клас
+        {
+            private double x, y;
+            public Rectangle(double x, double y) : base(x,y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public double Length()
+            {
+                return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            }
+            public override void Area()
+            {
+                a = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+                b = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+                double s = a * b;
+                Console.WriteLine("Area is:" + s);
+            }
+            public override void Perimeter()
+            {
+                a = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+                b = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+                double p = 2 * (a + b);
+                Console.WriteLine("Perimetr is:" + p);
+            }
+        }
+
+        class Circle : Figures//похідний клас
+        {
+            private double r;
+            public Circle(double x) : base(x)
+            {
+                r = x;
+            }
+            public override void Area()
+            {
+                double s = pi * r * r;
+                Console.WriteLine("Area is:" + s);
+            }
+            public override void Perimeter()
+            {
+                double p = 2 * pi * r;
+                Console.WriteLine("Perimetr is:" + p);
+            }
+        }
+
+        static void Solve()
+        {
+            Console.WriteLine("Kovalenko Yura IS-92");
+            Rectangle a = new Rectangle(3, 4);
+            Console.WriteLine("For Rectangle");
+            a.Area();
+            a.Perimeter();
+            Circle b = new Circle(5);
+            Console.WriteLine("For Circle");
+            b.Area();
+            b.Perimeter();
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Solve();
+        }
+    }
+}
